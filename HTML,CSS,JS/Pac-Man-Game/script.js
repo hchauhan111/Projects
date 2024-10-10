964d006d6675f8c38b1b35277048d89edd0c1b2a
Himanshu Chauhan <himanshuchauhan2004@gmail.com>
2024-10-10 16:40:09 +0530

    Created player and its movement and some additional changes made

--- HTML,CSS,JS/Pac-Man-Game/script.js
+++ HTML,CSS,JS/Pac-Man-Game/script.js
@@ -22,6 +22,34 @@ class Boundary {
   }
 }
 
+class Player {
+  constructor({ position }) {
+    this.position = position;
+    this.radius = 15;
+    this.speed = 4;
+    this.dx = 0;
+    this.dy = 0;
+  }
+
+  draw() {
+    cxt.beginPath();
+    cxt.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    cxt.fillStyle = "yellow";
+    cxt.fill();
+    cxt.closePath();
+  }
+
+  update = () => {
+    this.draw();
+    this.move();
+  };
+
+  move() {
+    this.position.x += this.dx;
+    this.position.y += this.dy;
+  }
+}
+
 const map = [
   ["-", "-", "-", "-", "-", "-"],
   ["-", " ", " ", " ", " ", "-"],
@@ -30,18 +58,67 @@ const map = [
   ["-", "-", "-", "-", "-", "-"],
 ];
 
-map.forEach((row, i) => {
-  row.forEach((segment, j) => {
-    switch (segment) {
-      case "-":
-        const boundary = new Boundary({
-          position: {
-            x: Boundary.width * j,
-            y: Boundary.height * i,
-          },
-        });
-        boundary.draw();
-        break;
-    }
+function drawMap() {
+  map.forEach((row, i) => {
+    row.forEach((segment, j) => {
+      switch (segment) {
+        case "-":
+          const boundary = new Boundary({
+            position: {
+              x: Boundary.width * j,
+              y: Boundary.height * i,
+            },
+          });
+          boundary.draw();
+          break;
+      }
+    });
   });
+}
+
+const p1 = new Player({
+  position: {
+    x: Boundary.width + Boundary.width / 2,
+    y: Boundary.height + Boundary.height / 2,
+  },
 });
+
+function mainAnimation() {
+  cxt.clearRect(0, 0, canvas.width, canvas.height);
+  p1.update();
+  drawMap();
+  requestAnimationFrame(mainAnimation);
+}
+
+mainAnimation();
+
+function onKeydown(e) {
+  if (e.key === "ArrowRight") {
+    p1.dx = p1.speed;
+    p1.dy = 0;
+  } else if (e.key === "ArrowLeft") {
+    p1.dx = -p1.speed;
+    p1.dy = 0;
+  } else if (e.key === "ArrowUp") {
+    p1.dy = -p1.speed;
+    p1.dx = 0;
+  } else if (e.key === "ArrowDown") {
+    p1.dy = p1.speed;
+    p1.dx = 0;
+  }
+}
+
+function onKeyup(e) {
+  if (
+    e.key === "ArrowRight" ||
+    e.key === "ArrowLeft" ||
+    e.key === "ArrowUp" ||
+    e.key === "ArrowDown"
+  ) {
+    p1.dx = 0;
+    p1.dy = 0;
+  }
+}
+
+document.addEventListener("keydown", onKeydown);
+document.addEventListener("keyup", onKeyup);
